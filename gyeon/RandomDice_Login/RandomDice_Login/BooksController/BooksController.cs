0fb24dc7f4fe8b18ce3e7c2dcab7cd8b328bee21
gyujun9403 <gujun112@naver.com>
2022-10-09 19:46:12 +0900

    #5 - MariaDB 실습

---
+++ gyeon/RandomDice_Login/RandomDice_Login/BooksController/BooksController.cs
@@ -0,0 +1,75 @@
+﻿using RandomDice_Login.Models;
+using RandomDice_Login.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+
+namespace BooksApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BookService_Maira _bookService;
+
+        public BooksController(BookService_Maira bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Book_Maria> Get(string id)
+        {
+            var book = _bookService.GetBook(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        
+        }
+
+        [HttpPost]
+        public JsonResult Create([FromBody]Book_Maria book)
+        {
+            if (_bookService.InsertBook(book) == false)
+            {
+                return new JsonResult("실패");
+            }
+
+            return new JsonResult("성공");
+        }
+
+        //[HttpPut("{id:length(24)}")]
+        //public IActionResult Update(string id, Book bookIn)
+        //{
+        //    var book = _bookService.Get(id);
+
+        //    if (book == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    _bookService.Update(id, bookIn);
+
+        //    return NoContent();
+        //}
+
+        //[HttpDelete("{id:length(24)}")]
+        //public IActionResult Delete(string id)
+        //{
+        //    var book = _bookService.Get(id);
+
+        //    if (book == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    _bookService.Remove(id);
+
+        //    return NoContent();
+        //}
+    }
+}
