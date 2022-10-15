b56d3134bfa5d01471dc2c2840e6c8a4aaafa72f
kimchijinju <hanbimkim@gmail.com>
2022-10-15 19:22:50 +0900

    migration .net 6.0

---
+++ Randomdice_API/Randomdice_API/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RandomDice_Login.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace RandomDice_Login.BooksController
+{
+    [Route("[controller]")] // login/~인 URL은 이 컨트롤러로 라우팅된다.
+    public class LoginController
+    {
+        [HttpPost]
+        [Route("login")]
+        public JsonResult LoginProc([FromBody] LoginModel login)
+        {
+            try
+            {
+                login.Login();
+            }
+            catch (Exception e)
+            {
+                return new JsonResult(e.ToString());
+            }
+            return new JsonResult("성공");
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public JsonResult RegisterRoc([FromBody] UserModel user)
+        {
+            try
+            {
+                user.Register();
+            }
+            catch (Exception e)
+            {
+                return new JsonResult(e.ToString());
+            }
+            return new JsonResult("성공");
+        }
+    }
+}
---
+++ Randomdice_API/Randomdice_API/Program.cs
@@ -0,0 +1,20 @@
+﻿using MySqlConnector;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+
+builder.Services.AddControllers();
+builder.Services.AddTransient<MySqlConnection>(_ => new MySqlConnection(System.Environment.GetEnvironmentVariable("CONNECTION_STRING")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.Run();
