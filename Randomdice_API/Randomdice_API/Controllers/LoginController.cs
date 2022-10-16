f3f771ae7b66e533e99034344ddd8210cbe7d673
gyujun9403 <gujun112@naver.com>
2022-10-17 01:27:53 +0900

    test:EF

--- Randomdice_API/Randomdice_API/Controllers/LoginController.cs
+++ Randomdice_API/Randomdice_API/Controllers/LoginController.cs
@@ -1,44 +1,91 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RandomDice_Login.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Randomdice_API.Data;
 
 
 namespace RandomDice_Login.BooksController
 {
+
     [Route("[controller]")] // login/~인 URL은 이 컨트롤러로 라우팅된다.
     public class LoginController
     {
-        [HttpPost]
-        [Route("login")]
-        public JsonResult LoginProc([FromBody] LoginModel login)
+        // DI
+        private UserContext _userContext;
+
+        public LoginController(UserContext userContext)
         {
-            try
-            {
-                login.Login();
-            }
-            catch (Exception e)
-            {
-                return new JsonResult(e.ToString());
-            }
-            return new JsonResult("성공");
+            _userContext = userContext;
+        }
+        //[HttpPost]
+        //[Route("login")]
+        //public JsonResult LoginProc([FromBody] LoginModel login)
+        //{
+        //    try
+        //    {
+        //        login.Login();
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        return new JsonResult(e.ToString());
+        //    }
+        //    return new JsonResult("성공");
+        //}
+
+        public JsonResult test()
+        {
+            return new JsonResult("hiii");
         }
 
-        [HttpPost]
-        [Route("register")]
-        public JsonResult RegisterRoc([FromBody] UserModel user)
+        [HttpGet("{id}")]
+        [Route("info")]
+        public UserModel? getUserInof(string id)
         {
-            try
-            {
-                user.Register();
-            }
-            catch (Exception e)
-            {
-                return new JsonResult(e.ToString());
-            }
-            return new JsonResult("성공");
+            UserModel? user = _userContext.UserModels.FirstOrDefault(predicate: s => s.UserName == id);
+            return user;
         }
+
+        //[HttpPost]
+        //[Route("register")]
+        //public JsonResult RegisterRoc([FromBody] UserModel user)
+        //{
+        //    try
+        //    {
+        //        user.Register();
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        return new JsonResult(e.ToString());
+        //    }
+        //    return new JsonResult("성공");
+        //}
+        //[HttpPost]
+        //[Route("login")]
+        //public JsonResult LoginProc([FromBody] LoginModel login)
+        //{
+        //    try
+        //    {
+        //        login.Login();
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        return new JsonResult(e.ToString());
+        //    }
+        //    return new JsonResult("성공");
+        //}
+
+        //[HttpPost]
+        //[Route("register")]
+        //public JsonResult RegisterRoc([FromBody] UserModel user)
+        //{
+        //    try
+        //    {
+        //        user.Register();
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        return new JsonResult(e.ToString());
+        //    }
+        //    return new JsonResult("성공");
+        //}
     }
 }
---
+++ Randomdice_API/Randomdice_API/Data/UserContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RandomDice_Login.Models;
+
+namespace Randomdice_API.Data
+{
+    public class UserContext : DbContext // DB와 상호작용하는 게이트웨이 역할.
+    {
+        // 외부코드()가 구성을 전달할 수 있고, 동일한 DbContext코드를 테스트코드-프로덕션코드-타공급자끼리도 사용 가능 ->?????
+        public UserContext (DbContextOptions<UserContext> options) : base(options)
+        {
+
+        }
+
+        // DB에 생성될 테이블 들. 테이블의 이름은 기본적으로 탬플릿 인자의 것+s형태를 띄는게 좋...다?
+        public DbSet<UserModel> UserModels { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserModel>().ToTable("t_user");
+        }
+    }
+}
